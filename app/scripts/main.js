a4c6e88564f71a2b7097d9623a7b8691161c1df0
Chris Camaratta <chris.camaratta@storycloud.co>
2015-08-29 17:00:50 -0700

    Styled using bootstrap 'cos it was ugly and added some instructions.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,8 @@
-!function () {
+(function ($, createPhotoSphereViewer) {
     'use strict';
 
-    var container = $('#photo-viewer');
+    var $container = $('.photo-viewer');
+    var $imageTitle = $('.media-heading');
 
     var checkIsPhotoSphere = function (img, callback) {
         if (!window.XMLHttpRequest) {
@@ -10,14 +11,14 @@
 
         var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function () {
-            if (xhr.readyState == 4 && xhr.status == 200) {
+            if (xhr.readyState === 4 && xhr.status === 200) {
                 var binary = xhr.responseText;
                 var metaStart = binary.indexOf('<x:xmpmeta');
                 var metaEnd = binary.indexOf('</x:xmpmeta>');
                 var data = binary.substring(metaStart, metaEnd);
 
                 // No data retrieved
-                if (metaStart == -1 || metaEnd == -1 || data.indexOf('GPano:') == -1) {
+                if (metaStart === -1 || metaEnd === -1 || data.indexOf('GPano:') === -1) {
                     callback(false);
                 } else {
                     callback(true);
@@ -30,33 +31,39 @@
     };
 
     var showImage = function (img) {
-        if (img instanceof jQuery.Event) {
+        if (img instanceof $.Event) {
             img = this;
         }
         var $img = $(img).clone();
 
-        container.empty().append($img);
+        $imageTitle.text(img.src.substring(img.src.lastIndexOf('/') + 1));
+        $container
+            .empty()
+            .removeClass('psv-container')
+            .append($img);
 
         checkIsPhotoSphere(img, function (isPhotoSphere) {
             if (!isPhotoSphere) {
+                $imageTitle.append('<small class="pull-right">This has no "GPano" data.</small>')
                 return;
             }
 
-            var $sphere = $('<img src="images/logo_photosphere.png" class="photo-sphere">');
+            var $sphere = $('<img src="images/logo_photosphere.png" class="photo-sphere-icon">');
 
             $sphere.click(function () {
-                container.empty();
-                PhotoSphereViewer({
-                    container: container[0],
+                $container.empty();
+                createPhotoSphereViewer({
+                    container: $container[0],
                     panorama: img.src
                 });
             });
 
-            container.append($sphere);
+            $imageTitle.append('<small class="pull-right">Click the icon to go 3D!</small>')
+            $container.append($sphere);
         });
     };
 
-    $('#gallery').on('click', 'img', showImage);
+    $('.gallery').on('click', 'img', showImage);
 
-    showImage($('#gallery img')[0]);
-}();
+    showImage($('.gallery img')[0]);
+}(window.jQuery, window.PhotoSphereViewer));
